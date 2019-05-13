824ba62d2c5c78d166749dc5a0001ae61a74e4d1
Otshelnik-Fm <Otshelnik-Fm@yandex.ru>
2019-05-14 00:27:02 +0400

    v1.1

--- index.php
+++ index.php
@@ -12,9 +12,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
  */
 
+// great packer http://refresh-sf.com/
+
 add_action( 'rcl_enqueue_scripts', 'siem_style', 10 );
 function siem_style() {
-    rcl_enqueue_style( 'siem_style', rcl_addon_url( 'assets/css/siem-style.css', __FILE__ ) );
+    rcl_enqueue_style( 'siem_style', rcl_addon_url( 'assets/css/siem-style-min.css', __FILE__ ) );
 }
 
 add_action( 'init', 'siem_global_emoji', 10 );
@@ -24,7 +26,7 @@ function siem_global_emoji() {
     $wpsmiliestrans = Array(); // обнулим вп-шный массив смайлов
     /**/
 
-    // новые https://apps.timwhitlock.info/emoji/tables/unicode
+    // новые http://www.unicode.org/emoji/charts/full-emoji-list.html
     // https://emojipedia.org/twitter/
     // дубли нам нужны
     $smilies[':-)']       = "&#x1f642;";     // улыбка
@@ -73,9 +75,12 @@ function siem_global_emoji() {
     $smilies['&#x1f62d;'] = "&#x1f62d;";     // плачет
     $smilies['&#x1f628;'] = "&#x1f628;";     // больной
     $smilies['&#x1f637;'] = "&#x1f637;";     // в маске
+    $smilies['&#x1f912;'] = "&#x1f912;";     // с градусником
     $smilies['&#x1f910;'] = "&#x1f910;";     // рот на замке
     $smilies['&#x1f92b;'] = "&#x1f92b;";     // тс...
+    $smilies['&#x1f914;'] = "&#x1f914;";     // поразмыслим
 
+    $smilies['&#x1f97a;'] = "&#x1f97a;";     // умоляющий
     $smilies['&#x1f607;'] = "&#x1f607;";     // ангелочек
     $smilies['&#x1f62c;'] = "&#x1f62c;";     // гримасничает
     $smilies['&#x1f634;'] = "&#x1f634;";     // спит
@@ -87,6 +92,8 @@ function siem_global_emoji() {
     $smilies['&#x1f383;']        = "&#x1f383;";          // хеллоуин
     $smilies['&#x1f921;']        = "&#x1f921;";          // клоун
     $smilies['&#x1f9d9;']        = "&#x1f9d9;";          // волшебник
+    $smilies['&#x1f9da;']        = "&#x1f9da;";          // фея
+    $smilies['&#x1F9DF;']        = "&#x1F9DF;";          // зомби
     $smilies['&#x1f332;']        = "&#x1f332;";          // елка
     $smilies['&#x1f384;']        = "&#x1f384;";          // наряженная елка
     $smilies['&#x2744;']         = "&#x2744;";           // снежинка
@@ -115,13 +122,18 @@ function siem_global_emoji() {
     $smilies['&#x1f352;'] = "&#x1f352;";     // вишенки
     $smilies['&#x1f353;'] = "&#x1f353;";     // клубнички
     $smilies['&#x1f355;'] = "&#x1f355;";     // пицца
+    $smilies['&#x1F9C0;'] = "&#x1F9C0;";     // сыр
     // emotions
     $smilies['&#x1f48b;'] = "&#x1f48b;";     // губы
     $smilies['&#x2764;']  = "&#x2764;";      // сердце
+    $smilies['&#x1f498;'] = "&#x1f498;";     // стрела в сердце
     $smilies['&#x1f495;'] = "&#x1f495;";     // 2 сердца
     $smilies['&#x1f48c;'] = "&#x1f48c;";     // сердце письмо
     $smilies['&#x1f494;'] = "&#x1f494;";     // разбито
     $smilies['&#x270c;']  = "&#x270c;";      // victory
+    $smilies['&#x1f44d;'] = "&#x1f44d;";      // палец вверх
+    $smilies['&#x1f44e;'] = "&#x1f44e;";      // палец вниз
+    $smilies['&#x1f91d;'] = "&#x1f91d;";      // по рукам
     // symbols
     $smilies['&#x2795;']  = "&#x2795;";      // плюс
     $smilies['&#x2714;']  = "&#x2714;";      // чекаут
@@ -132,30 +144,40 @@ function siem_global_emoji() {
     $smilies['&#x26a1;']  = "&#x26a1;";      // молния
     $smilies['&#x1f4cc;'] = "&#x1f4cc;";     // закрепить
     $smilies['&#x1f6ab;'] = "&#x1f6ab;";     // стоп
+    $smilies['&#x1F5D1;'] = "&#x1F5D1;";     // корзина
+    $smilies['&#x1F50e;'] = "&#x1F50e;";     // поиск - лупа
     $smilies['&#x1f4a1;'] = "&#x1f4a1;";     // идея
     $smilies['&#x1f4a3;'] = "&#x1f4a3;";     // бомба
     $smilies['&#x1f4a5;'] = "&#x1f4a5;";     // взрыв
+    $smilies['&#x1F389;'] = "&#x1F389;";     // хлопушка
     $smilies['&#x1f381;'] = "&#x1f381;";     // подарок
+    $smilies['&#x1F382;'] = "&#x1F382;";     // торт
     $smilies['&#x1f4b0;'] = "&#x1f4b0;";     // мешок денег
     $smilies['&#x1f4b5;'] = "&#x1f4b5;";     // наличные
     $smilies['&#x1f4b2;'] = "&#x1f4b2;";     // бакс
     $smilies['&#x270f;']  = "&#x270f;";      // ручка
     $smilies['&#x1f4dd;'] = "&#x1f4dd;";     // блокнот
+    $smilies['&#x1F517;'] = "&#x1F517;";     // ссылка
     $smilies['&#x1f528;'] = "&#x1f528;";     // молоток
     $smilies['&#x1f527;'] = "&#x1f527;";     // гаечный ключ
+    $smilies['&#x2699;']  = "&#x2699;";      // настройки
     $smilies['&#x1f552;'] = "&#x1f552;";     // время
     $smilies['&#x1f4a4;'] = "&#x1f4a4;";     // спать
     /**/
 
     $smilies['&#x1f1f7;&#x1f1fa;'] = "&#x1f1f7;&#x1f1fa;";   // ru flag
+    $smilies['&#x1F1FA;&#x1F1F8;'] = "&#x1F1FA;&#x1F1F8;";   // usa flag
     $smilies['&#x2709;']           = "&#x2709;";             // конверт
     $smilies['&#x1f3c6;']          = "&#x1f3c6;";            // кубок
+    $smilies['&#x1F396;']          = "&#x1F396;";            // медаль
     /**/
 
     // animals
     $smilies['&#x1f425;'] = "&#x1f425;";     // цыпленок
     $smilies['&#x1f41f;'] = "&#x1f41f;";     // рыба
     $smilies['&#x1f437;'] = "&#x1f437;";     // свин
+    $smilies['&#x1f41e;'] = "&#x1f41e;";     // bug
+    $smilies['&#x1f577;'] = "&#x1f577;";     // паук
     $smilies['&#x1f47d;'] = "&#x1f47d;";     // инопланетянин
     $smilies['&#x1f480;'] = "&#x1f480;";     // череп
     $smilies['&#x1f47b;'] = "&#x1f47b;";     // призрак
@@ -166,12 +188,16 @@ function siem_global_emoji() {
     $smilies['&#x2600;']          = "&#x2600;";             // солнце
     $smilies['&#x1f334;']         = "&#x1f334;";            // пальма
     $smilies['&#x1f33c;']         = "&#x1f33c;";            // цветок
+    $smilies['&#x1f490;']         = "&#x1f490;";            // букет
+    $smilies['&#x1f341;']         = "&#x1f341;";            // кленовый лист
+    $smilies['&#x1f342;']         = "&#x1f342;";            // листопад
 
     $smilies['&#x26fa;']  = "&#x26fa;";     // кемпинг палатка
     $smilies['&#x23f0;']  = "&#x23f0;";     // часы
     $smilies['&#x26bd;']  = "&#x26bd;";     // мяч
     $smilies['&#x2b50;']  = "&#x2b50;";     // звезда
     $smilies['&#x1f4af;'] = "&#x1f4af;";    // 100%
+    $smilies['&#x1f5e8;'] = "&#x1f5e8;";    // комментарий
 
     $wpsmiliestrans = apply_filters( 'siem_emoji', $smilies );
 
